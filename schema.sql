6e11360f8fa52ffcf08ef43b80d61ddeda1d0f4d
Tristan Livengood <tlivengood55@gmail.com>
2023-12-11 17:42:11 -0500

    added some sql data and made sure stuff is named correctly through out the files

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE DATABASE employee_tracker_db;
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
 
 INSERT INTO department (id, name) VALUES
   (1, 'Department1'),
